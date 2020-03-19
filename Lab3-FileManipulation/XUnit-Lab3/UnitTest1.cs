6be045caf287a15f7b951f6e90f44408064970e8
Sue Tarazi <suemachtley@gmail.com>
2020-03-18 17:20:49 -0700

    initial setup

---
+++ Lab3-FileManipulation/XUnit-Lab3/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Lab3_FileManipulation;
+
+namespace XUnit_Lab3
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
