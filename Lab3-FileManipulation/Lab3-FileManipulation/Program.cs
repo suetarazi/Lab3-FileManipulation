bfaa8eeb266bf4190fe31276700fe959a11580ac
Sue Tarazi <suemachtley@gmail.com>
2020-03-19 22:11:05 -0700

    code is working

--- Lab3-FileManipulation/Lab3-FileManipulation/Program.cs
+++ Lab3-FileManipulation/Lab3-FileManipulation/Program.cs
@@ -5,30 +5,22 @@ namespace Lab3_FileManipulation
 {
     public class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            //WriteToAFile();
+            WriteToAFile();
+            ViewList();
             //addItem();
             //removeItem();
             //Exit();
-            DeleteLine("beer");
-            ViewList();
         }
-
-        public static void WriteToAFile()
+    
+    static void WriteToAFile()
         {
             string path = "../../../lab3list.txt";
             File.WriteAllText(path, "toilet paper");
         }
-
-        public static void WriteError(string error)
-        {
-            string path = "../../../lab3errorLog.txt";
-            File.WriteAllText(path, error);
-
-        }
-
-        public static void ViewList()
+    
+    static void ViewList()
         {
             try
             {
@@ -45,45 +37,12 @@ namespace Lab3_FileManipulation
             catch (FileNotFoundException e)
             {
                 Console.WriteLine("Your file is not available", e.Message);
-
-                WriteError(e.Message);
-
             }
             finally
             {
                 Console.WriteLine("Process Complete");
             }
-        }
-        public static string[] ReadAllLines()
-        {
-            string path = "../../../lab3list.txt";
-            string[] myText = File.ReadAllLines(path);
-            return myText;
-        }
-        public static void DeleteLine(string ItemRemoved)
-        {
-            string[] words = ReadAllLines();
-            string[] newWords = new string[words.Length - 1];
-            int j = 0;
-            for (int i = 0; i < words.Length; i++)
-            {
-                if (words[i] != ItemRemoved)
-                {
-                    newWords[j] = words[i];
-                    j++;
                 }
-                WriteToAFile(newWords);
             }
         }
-        static void WriteToAFile(string[] contents)
-        {
-            string path = "../../../lab3list.txt";
-            File.WriteAllLines(path, contents);
-
-
-        }
-    }
-
-
-}
 
